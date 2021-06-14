1b13b92f7f082e9ed2aa8203ea02afd55b3c126b
1234 <209410306@gms.tku.edu.tw>
2021-06-14 18:52:30 +0800

    fix bug

--- pages/school/timetable.js
+++ pages/school/timetable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './timetable.module.css'
+import styles from '../../styles/timetable.module.css'
 export default function resume() {
     return (
         <body>
