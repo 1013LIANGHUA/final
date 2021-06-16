dcee44875bfc2d0925a9227883b1c151f97b845f
1234 <209410306@gms.tku.edu.tw>
2021-06-16 23:35:04 +0800

    修改了封面設計

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,38 @@
 import Head from 'next/head';
 import Layout from '../components/Layout';
-import styles from './index.module.css'
+import styles from './index.module.css';
 export default function HomePage() {
   return (
     <Layout description='Welcome to me'>
-      <div className={styles.img}>
-        <img src="https://res.cloudinary.com/dkla3sb3q/image/upload/v1623584064/large_S_37609491_0bdbf75786.jpg" alt="profile_card"/>
+      {/* <div className={styles.container}>
+        <div className={styles.card}>
+          <div >
+            <img
+              src='https://res.cloudinary.com/dkla3sb3q/image/upload/v1623584064/large_S_37609491_0bdbf75786.jpg'
+              alt='profile_card'
+            />
+          </div>
+          <div className={styles.info}>
+            <button></button>
+            <p></p>
+          </div>
+        </div>
+      </div>   */}
+      <div className={styles.container}>
+        <div className={styles.box1}>
+          <h1>陳亮華</h1>
+          <img
+            src='https://res.cloudinary.com/dkla3sb3q/image/upload/v1623584064/large_S_37609491_0bdbf75786.jpg'
+            alt='profile_card'
+          />
+          <p>
+            淡江大學 資訊工程學系
+          </p>
+          <br />
+          <a href='http://www.designshock.com/'> DesignShock.com </a>
+        </div>
       </div>
-  
-    
-     
-</Layout>
 
+    </Layout>
   );
 }
-
