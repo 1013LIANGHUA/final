d19e3ed9ec150fe3f0b70b89d81ce505f746575c
1234 <209410306@gms.tku.edu.tw>
2021-06-17 07:42:45 +0800

    修改自行顏色、背景圖片擺放位置

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p></p>
+      <p>js Final project 2021</p>
       {/* <p>
         <Link href='/about'>About This Project</Link>
       </p> */}
